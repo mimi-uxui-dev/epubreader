96fd401c59ef52ca2d8eef2b76ae1156158d77f5
mimi-uxui-dev <uxui.mimi@gmail.com>
2024-01-13 12:26:06 +0100

    basic file reading

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import { ReactReader } from "react-reader";
 
 function App() {
+  const [location, setLocation] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: "100vh" }}>
+      <ReactReader url="files/book1.epub" />
     </div>
   );
 }
